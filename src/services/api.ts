ad1aeb7af710b2e063267990f7b563786d7a09e8
dev <devgroup006@gmail.com>
2025-05-10 09:13:26 -0700

    added api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Read the base URL from an env-var (see below)
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 // Create an Axios instance
 const api = axios.create({
